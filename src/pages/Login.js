8c05092044c23f61d3a1105fa7b12b5d50c64281
rashedInt32 <parvez08eee@gmail.com>
2019-09-18 15:33:27 +0600

    decoding auth token and set it to localstorage

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
+import { Link } from 'react-router-dom';
 
 import { config } from '../config';
 import { http } from '../http';
+
+import Input from '../components/forms/Input';
 
 const authUri = `${config.baseUrl}/auth`;
 
@@ -11,35 +14,66 @@ function Login() {
     password: ''
   });
 
-  const onInputChage = e => {
+  const [authError, setAuthError] = useState({
+    error: false,
+    msg: ''
+  })
+
+  const onInputChange = e => {
     const { name, value } = e.target;
     setAuthData({ ...authData, [name]: value });
   };
 
   const onSubmitForm = async (e) => {
     e.preventDefault();
+
     const { data } = await http.post(authUri, authData);
+    if (data.error)
+      return setAuthError({
+        error: true,
+        msg: data.msg
+      });
+
+    const decodeToken = JSON.parse(window.atob(data.token.split(".")[1]));
+    localStorage.setItem('token', data.token);
+    localStorage.setItem('user', JSON.stringify(decodeToken));
     console.log(data);
+
   };
 
   return (
-    <form action="" onSubmit={onSubmitForm}>
-      <label>email</label>
-      <input
-        type="text"
-        name="email"
-        value={authData.email}
-        onChange={onInputChage}
-      />
-      <label>Password</label>
-      <input
-        type="password"
-        name="password"
-        value={authData.password}
-        onChange={onInputChage}
-      />
-      <button type="submit">Login</button>
-    </form>
+    <div className="container">
+      <div className="row">
+        <form
+          action="#"
+          className="col-md-4 offset-md-4 auth-form"
+          onSubmit={onSubmitForm}
+        >
+          <div className="auth-card">
+            <h3>Login</h3>
+            <Input
+              type="email"
+              label="Email"
+              value={authData.email}
+              onChange={onInputChange}
+              name="email"
+            />
+            <Input
+              type="password"
+              label="Password"
+              value={authData.password}
+              onChange={onInputChange}
+              name="password"
+            />
+            {authError.error ? <div className="alert alert-danger" role="alert">
+              {authError.msg}
+            </div> : ''}
+            <button className="btn btn-primary">Login</button>
+            <p className="pt-3">Don't have an account? <Link to="/register">Register</Link></p>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
 
