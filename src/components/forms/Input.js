8033f2896cd5b7f071d05c5b1d6e51e34095d079
rashedInt32 <parvez08eee@gmail.com>
2019-09-21 00:48:26 +0600

    Add scss template from another and implemnt login page with new UI

--- src/components/forms/Input.js
+++ src/components/forms/Input.js
@@ -2,13 +2,14 @@ import React from "react";
 
 const Input = props => (
   <div className="form-group">
-    <label>{props.label}</label>
+
     <input
       value={props.value}
       type={props.type}
       onChange={props.onChange}
       name={props.name}
-      className="form-control"
+      className={props.className}
+      placeholder={props.placeholder}
     />
   </div>
 );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -47,7 +47,68 @@ function Login() {
   };
 
   return (
-    <div className="container">
+    <div>
+      <div className="container-fluid page-body-wrapper full-page-wrapper">
+        <div className="content-wrapper d-flex align-items-center auth px-0">
+          <div className="row w-100 mx-0">
+            <div className="col-lg-4 mx-auto">
+              <div className="auth-form-light text-left py-5 px-4 px-sm-5">
+                <div className="brand-logo">
+                  {/* Logo should be here with image tag */}
+                </div>
+                <h4>Hello! let's get started</h4>
+                <h6 className="font-weight-light">Sign in to continue.</h6>
+                <form className="pt-3" onSubmit={onSubmitForm}>
+                  <div className="form-group">
+                    <Input
+                      type="email"
+                      value={authData.email}
+                      onChange={onInputChange}
+                      name="email"
+                      className="form-control form-control-lg"
+                      placeholder="Email"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <Input
+                      type="password"
+                      value={authData.password}
+                      onChange={onInputChange}
+                      name="password"
+                      className="form-control form-control-lg"
+                      placeholder="Password"
+                    />
+                  </div>
+                  <div className="mt-3">
+                    <button className="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn">
+                      Login
+                    </button>
+                  </div>
+                  <div className="my-2 d-flex justify-content-between align-items-center">
+                    <div className="form-check">
+                      <label className="form-check-label text-muted">
+                        <input type="checkbox" className="form-check-input" />
+                        Keep me signed in
+                      </label>
+                    </div>
+                    <a  className="auth-link text-black">
+                      Forgot password?
+                    </a>
+                  </div>
+                  <div className="text-center mt-4 font-weight-light">
+                    Don't have an account?{" "}
+                    <a href="register.html" className="text-primary">
+                      Create
+                    </a>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="container">
       <div className="row">
         <form
           action="#"
@@ -78,6 +139,7 @@ function Login() {
           </div>
         </form>
       </div>
+    </div> */}
     </div>
   );
 }
