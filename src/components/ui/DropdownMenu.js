e8427c97558cae2c22e766fb9a5ca72b840acd7c
rashedInt32 <parvez08eee@gmail.com>
2019-11-19 23:26:29 +0600

    Added outside click status

--- src/components/ui/DropdownMenu.js
+++ src/components/ui/DropdownMenu.js
@@ -1,15 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+
+import { useOnClickOutside } from "../../hooks/useClickOutside";
 
 const DropdownMenu = ({menus}) => {
   const [show, setShow] = useState(false);
   const showDropdwon = () => setShow(!show);
+  const el = useRef(null);
+
+  useOnClickOutside(el, () => setShow(false));
 
   return (
     <div className="dropdown dropright card-menu-dropdown">
       <button className="btn p-0" type="button" onClick={showDropdwon}>
         <i className="material-icons">more_vert</i>
       </button>
-      <div className={show ? "dropdown-menu show" : "dropdown-menu"}>
+      <div ref={el} className={show ? "dropdown-menu show" : "dropdown-menu"}>
         {
           menus.map((menu, i) =>
             <button
